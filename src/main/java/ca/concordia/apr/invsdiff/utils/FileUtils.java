0e785bab884444bb71172c5b232ea2e3e15b4bae
Bo Yang <boyang9602@gmail.com>
2019-12-12 13:57:08 -0800

    fix bug: filename too long, use hashcode to represent params

--- src/main/java/ca/concordia/apr/invsdiff/utils/FileUtils.java
+++ src/main/java/ca/concordia/apr/invsdiff/utils/FileUtils.java
@@ -3,10 +3,21 @@ package ca.concordia.apr.invsdiff.utils;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtils {
-	
+	private static final Pattern paramPattern = Pattern.compile("^(.*)\\((.+)\\)$");
 	public static void writeTo(String filename, String content) throws IOException {
+		Matcher m = paramPattern.matcher(filename);
+		if (m.matches()) {
+			StringBuffer sb = new StringBuffer();
+			sb.append(m.group(1));
+			for (String param : m.group(2).replace(" ", "").split(",")) {
+				sb.append(param.hashCode()).append(',');
+			}
+			filename = sb.toString();
+		}
 		File file = new File(filename);
 		file.getParentFile().mkdirs();
 		PrintWriter writer = new PrintWriter(file);
